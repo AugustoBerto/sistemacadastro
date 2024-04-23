ffc46b983d87ccc51848b191c1c21a4364163fa5
_w3ndell <academy.wendell@gmail.com>
2024-04-23 14:31:49 -0300

    finalização da pagina de login

--- assets/js/login.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const loginContainer = document.getElementById("login-container");
-
-const moveOverlay = () => loginContainer.classList.add("move");
-
-document.getElementById("open-register").addEventListener("click", moveOverlay);
