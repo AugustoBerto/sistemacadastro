782e953ba693861b35e54e0f6c2bd84033a305e4
AugustoBerto <z3augustoberto@gmail.com>
2024-04-29 13:06:42 -0300

    Identação/Correção de bugs

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 const loginContainer = document.getElementById('login-container')
 
@@ -9,3 +9,5 @@ document.getElementById('open-login').addEventListener('click', moveOverlay)
 
 document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
 document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
+
+// :)
